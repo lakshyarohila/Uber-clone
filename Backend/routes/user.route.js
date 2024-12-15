5370d3dfc44a6e0a34bea4d9ca4a278661caf445
lakshyarohila <lakshyarohila99@gmail.com>
2024-12-15 18:50:12 +0530

    user login done

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -8,4 +8,8 @@ router.post("/register", [
   body("fullname.firstname").isLength({ min: 3 }).withMessage("First Name must be at least 3 character long"),
 ],userController.registerUser);
 
+router.post('/login',[body('email').isEmail().withMessage('Invalid Email'),
+  body('password').isLength({min:3}).withMessage('Password Must be 6 Character long')
+],userController.loginUser)
+
 module.exports = router;
