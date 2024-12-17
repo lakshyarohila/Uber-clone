ef9c457506849e8dd7597aa7769f4bc937943f4b
lakshyarohila <lakshyarohila99@gmail.com>
2024-12-17 19:33:41 +0530

    captain register route

--- Backend/app.js
+++ Backend/app.js
@@ -5,7 +5,8 @@ const cors = require('cors')
 const app = express();
 const cookieParser = require('cookie-parser')
 const connectDB = require('./db/db.js');
-const Userrouter = require('./routes/user.route.js')
+const Userrouter = require('./routes/user.route.js');
+const captaionRouter = require('./routes/captaion.route.js');
 connectDB();
 app.use(cors());
 app.use(express.json());
@@ -16,6 +17,7 @@ app.get('/',(req,res) => {
     res.send('Hello World')
 });
 app.use('/users',Userrouter);
+app.use('/captains',captaionRouter);
 
 
 
